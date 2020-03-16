ff934644e861642774a435120f75133b179e0c50
payal <payal.priyadarshini@gmail.com>
2020-03-16 18:44:50 +0000

    working on estimateform

--- dist/js/estimateform.js
+++ dist/js/estimateform.js
@@ -16,9 +16,6 @@
 
 (function(){
 
-
-
-
     /* Initializing variable*/
     let total = 0;
     let x = 0, y = 0;
@@ -60,8 +57,6 @@
     /* Getting the querystring*/
     let id = getParameterByName('id');
 
-    /* Getting the querystring*/
-    let id = getParameterByName('id');
 
     //onload initialiser
     window.onload = init;
@@ -139,7 +134,7 @@
     {
         for(let j=0; j< destinations.length - 1; j++)
         {
-            if(id == j+1)
+            if(id === j+1)
                 dispDestD.innerHTML = destinations[j];
         }
 
@@ -156,7 +151,7 @@
 
         for(let j=0; j< destinations.length - 1; j++)
         {
-            if(id == j+1) {
+            if(id === j+1) {
                 dispDestD.innerHTML = destinations[j];
                 dispFliBook.innerHTML = "Your flight booking fee: £ " + flightBookFee[j];
                 total = total + flightBookFee[j];
@@ -211,7 +206,7 @@
 
             for(let j=0; j< destinations.length - 1; j++)
             {
-                if(id == j+1) {
+                if(id === j+1) {
                     dispDestD.innerHTML = destinations[j];
                     dispOne.innerHTML = "One way to " + destinations[j] + " will cost £ " + oneWayFee[j];
                     total = total - y;
@@ -228,7 +223,7 @@
 
             for(let j=0; j< destinations.length - 1; j++)
             {
-                if(id == j+1) {
+                if(id === j+1) {
                     dispOne.innerHTML = "Return flight to " + destinations[j] + " will cost £ " + returnFee[j];
                     total = total - y;
                     y = returnFee[j];
@@ -258,7 +253,7 @@
 
             for(let j=0; j< destinations.length - 1; j++)
             {
-                if(id == j+1) {
+                if(id === j+1) {
                     dispHotelBook.innerHTML = "Your Hotel booking fee is : £ " + hotelBookFee[j];
                     dispSingleDouble.innerHTML = "Single room in "+ destinations[j]+" will cost £ "+ singleRoomFee[j];
                     /* Incase the user changes his preference for single/double room, the x value stores the old value of single/double room, hence it is subtracted from the total*/
@@ -275,7 +270,7 @@
 
             for(let j=0; j< destinations.length - 1; j++)
             {
-                if(id == j+1) {
+                if(id === j+1) {
                     dispHotelBook.innerHTML = "Your Hotel booking fee is : £ " + hotelBookFee[j];
                     dispSingleDouble.innerHTML = "Double room in "+ destinations[j]+ " will cost £ "+ doubleRoomFee[j];
                     /* Incase the user changes his preference for single/double room, the x value stores the old value of single/double room, hence it is subtracted from the total*/
@@ -291,138 +286,6 @@
 
     /**
 
-     * @name book
-     * @desc When 'Book Now' button is clicked, open page bookingForm.html in the same tab.
-     *
-     */
-    function book()
-    {
-        window.open("bookingForm.html?id="+ id, "_self");
-
-    } /* end book*/
-
-    /**
-     *  @name bindBtns
-     *  @desc Bind fields to event handler
-     */
-    function bindBtns() {
-        //Checking if button exists on page and then add eventhandler
-        if(destDd != null)destDd.addEventListener("change", initialize);
-        if(destDd != null)destDd.selectedIndex = id-1;
-        if(bookNow != null)bookNow.addEventListener("click", book);
-
-        //Reading radio buttons from collection for oneway/return and assigning to function
-        if(oneWay != null) {
-            for (let i = 0; i < oneWay.length; i++) {
-                oneWay[i].onclick = journey;
-            }
-        }
-        //Reading radio buttons from collection for single/double room and assigning to function
-        if(room != null) {
-            for (let j = 0; j < room.length; j++) {
-                room[j].onclick = single;
-            }
-        }
-
-    }// end bindBtns
-
-    /* FUNCTIONS*/
-
-    /**
-     * @name menu
-     * @desc creates the left menu and implements toggle
-     */
-    function menu()
-    {
-        /* THE LEFT MENU*/
-        /* Code taken from https://codepen.io/soulwire/pen/EKmwC*/
-        let $nigiri = $( '.nigiri' );
-
-        // Create Makisus
-        $nigiri.makisu({
-            selector: 'dd',
-            overlap: 0.85,
-            speed: 1.7
-        });
-
-        // Open all
-        $( '.list' ).makisu( 'open' );
-
-        // Toggle on click
-        $( '.toggle' ).on( 'click', function() {
-            $( '.list' ).makisu( 'toggle' );
-        });
-
-    }/* end menu*/
-
-    /**
-     * @name destDisp
-     * @desc displays the chosen destination in the dispDestD field
-     */
-    function destDisp()
-    {
-        for(let j=0; j< destinations.length - 1; j++)
-        {
-            if(id == j+1)
-                dispDestD.innerHTML = destinations[j];
-        }
-
-    }/*end destDisp*/
-
-    /**
-     * @name destChanged
-     * @desc To display the flight booking fee and the total when destination in
-     *  dropdown is changed
-     */
-    function destChanged()
-    {
-        let flightBookFee = [40, 50, 60, 30, 60,70];
-
-        for(let j=0; j< destinations.length - 1; j++)
-        {
-            if(id == j+1) {
-                dispDestD.innerHTML = destinations[j];
-                dispFliBook.innerHTML = "Your flight booking fee: £ " + flightBookFee[j];
-                total = total + flightBookFee[j];
-                dispTotal.innerHTML = "Your Total is:£ " + total;
-            }
-        }
-
-    }/* end destChanged*/
-
-
-    /**
-     * @name init
-     * @desc initialising function
-     */
-    function init() {
-        bindBtns();
-        menu();
-        if(destDd != null)destChanged();
-        if(dispDestD != null)destDisp();
-    }//end init
-
-    //onload initialiser
-    window.onload = init;
-
-     * @name getParameterByName
-     * @desc The value of id from querystring is extracted using regular expression
-     * @param: name, url
-     */
-    function getParameterByName(name,url)
-    {
-        if (!url) url = window.location.href;
-        name = name.replace(/[\[\]]/g, '\\$&');
-        const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-            results = regex.exec(url);
-        if (!results) return null;
-        if (!results[2]) return '';
-        return decodeURIComponent(results[2].replace(/\+/g, ' '));
-
-    }/* end getParameterByName*/
-
-
-    /**
      * @name book
      * @desc When 'Book Now' button is clicked, open page bookingForm.html in the same tab.
      *
